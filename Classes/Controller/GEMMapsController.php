94963514c51ffb52d3e473fb208009c84d6c4272
Christian Dilger <c.dilger@addorange.de>
2022-09-30 11:44:45 +0200

    [CHANGE] load assets only in plugin context

--- Classes/Controller/GEMMapsController.php
+++ Classes/Controller/GEMMapsController.php
@@ -3,6 +3,7 @@
 namespace RKW\RkwMaps\Controller;
 
 use TYPO3\CMS\Core\Page\PageRenderer;
+use TYPO3Fluid\Fluid\View\TemplateView;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Utility\DebuggerUtility;
 
@@ -51,10 +52,15 @@ class GEMMapsController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionControll
     protected $contentUid;
 
     /**
-     * @return void
+     * initializeAction
+     *
+     * @throws \TYPO3\CMS\Extbase\Mvc\Exception\NoSuchArgumentException
+     * @throws \TYPO3\CMS\Extbase\Mvc\Exception\UnsupportedRequestTypeException
+     * @throws \TYPO3\CMS\Extbase\Mvc\Exception\StopActionException
      */
-    protected function initializeAction()
+    public function initializeAction(): void
     {
+        parent::initializeAction();
         $this->getContentUid();
     }
 
@@ -65,6 +71,39 @@ class GEMMapsController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionControll
     {
         $this->contentUid = (int)$this->configurationManager->getContentObject()->data['uid'];
     }
+
+    /**
+     * Method overrides is base method as base method relies
+     * on objectManager to instantiate PageRenderer::class.
+     * But PageRenderer::class is initially instantiated by
+     * GeneralUtility::makeInstance, so the base method does not
+     * append the assets. This function may be removed, if the
+     * installation is upgraded to TYPO3 9.5.
+     *
+     * see https://forge.typo3.org/issues/89445
+     *
+     * @deprecated
+     *
+     * @param \TYPO3\CMS\Extbase\Mvc\RequestInterface $request
+     * @return void
+     */
+    protected function renderAssetsForRequest($request)
+    {
+        if (!$this->view instanceof TemplateView) {
+            return;
+        }
+        $pageRenderer = GeneralUtility::makeInstance(PageRenderer::class);
+        $variables = ['request' => $request, 'arguments' => $this->arguments];
+        $headerAssets = $this->view->renderSection('HeaderAssets', $variables, true);
+        $footerAssets = $this->view->renderSection('FooterAssets', $variables, true);
+        if (!empty(trim($headerAssets))) {
+            $pageRenderer->addHeaderData($headerAssets);
+        }
+        if (!empty(trim($footerAssets))) {
+            $pageRenderer->addFooterData($footerAssets);
+        }
+    }
+
 
     /**
      * action show
