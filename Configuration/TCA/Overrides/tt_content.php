7a62cd2849b89caaedd8e1d19527099fbd38b359
Christian Dilger <c.dilger@addorange.de>
2022-10-04 11:26:51 +0200

    [CHANGE] add examples and material through array for now

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -30,3 +30,13 @@ $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignat
     $pluginSignature,
     'FILE:EXT:'. $extKey . '/Configuration/FlexForms/Map.xml'
 );
+
+$extensionName = strtolower(\TYPO3\CMS\Core\Utility\GeneralUtility::underscoredToUpperCamelCase($extKey));
+$pluginName = strtolower('GEMMaps');
+$pluginSignature = $extensionName.'_'.$pluginName;
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'layout,select_key,pages';
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
+    $pluginSignature,
+    'FILE:EXT:'. $extKey . '/Configuration/FlexForms/GEMMap.xml'
+);
